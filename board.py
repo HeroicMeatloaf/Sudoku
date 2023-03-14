7b63d6355ded9ea7c5d62de21ae7dc7fb89289c0
HeroicMeatloaf <talexander1411@gmail.com>
2023-03-14 13:52:22 -0500

    Modified board.fillGrid() to both generate new puzzle as well as solve puzzle. No need for solver.py.

--- board.py
+++ board.py
@@ -12,6 +12,8 @@ def printGrid(grid):
    
 def fillGrid(grid = []): 
     if not grid:
+        # If grid is empty initialize it
+        print(f'Grid does not exist. Must initialize.')
         grid = [[0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
@@ -22,65 +24,142 @@ def fillGrid(grid = []):
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0]]
 
-    for row in range(9):
-        numbers = [x for x in range(1, 10)]
-        prevGrid = [row[:] for row in grid]
-        for col in range(9):
-            if grid[row][col]:
-                numbers.pop(numbers.index(grid[row][col]))
-                continue
+        print(sep)
+        printGrid(grid)
+        print(sep)
 
-            numLength = len(numbers)
+    for rowindex, row in enumerate(grid):
+        # For every (0, [0,0,0,0,0,0,0,0,0])
+
+        print(f'Starting row {rowindex}')
+
+        if 0 not in row:
+            print(f'This row is already filled in. Moving on to row number {rowindex + 1}')
+            # This row has already been filled in. Move on to next row
+            continue
+
+        # Take a copy of current grid before any changes are made.
+        prevGrid = [row[:] for row in grid]
+
+        for colindex, col in enumerate(row):
+            # For every column in row
+            print(f'Starting column {colindex}. Current number: {col}')
+
+            if grid[rowindex][colindex]:
+                # Non zero number. Move on to next column
+                print(f'Filled in. Moving on to next column {colindex + 1}')
+                continue
+            
+            # Initialize attempted to be an empty list
             attempted = []
 
             while True:
-                if len(attempted) == numLength:
-                    # print('TRIED ALL NUMBERS. RETURNING TO PREVIOUS STATE AND TRYING AGAIN')
-                    # print(sep)
+                # Recalculate numbers based on what's in row
+                numbers = [x for x in range(1,10) if x not in row and x not in attempted]
+                print(f'Current numbers to pick from: {numbers}')
+
+                if len(numbers) == 0:
+                    # Ran out of numbers to pick from. Back track to previous grid
+                    print(f'Ran out of valid options. Reverting back to previous grid')
+                    printGrid(prevGrid)
+                    print(sep)
                     grid = fillGrid(prevGrid)
                     return grid
 
-                num = random.choice([x for x in numbers if x not in attempted])
-                grid[row][col] = num
+                # Pick number at random from list of available numbers
+                # Assign that number to current location in grid
+                num = random.choice(numbers)
+                grid[rowindex][colindex] = num
+
+                print(f'Trying number {num} at {rowindex},{colindex}')
                 
-                # print(f'Currenlty trying {num} from {numbers}. This is attempt number {len(attempted) + 1}')
-                # print(f'Checking {num} for cell {row}-{col}')
-                if checker.checkNum(grid, row, col):
-                    # print(f'{num} at ({row}-{col}) is valid')
-                    numbers.pop(numbers.index(num))
-                    # printGrid(grid)
+                # Check to see if number passes
+                if checker.checkNum(grid, rowindex, colindex):
+                    # Passes - pop number from numbers and break out of while loop.
+                    # Should move on to next position in row
+                    print(f'{num} passes checks. Moving on to next position')
+                    print(sep)
+                    printGrid(grid)
+                    print(sep)
                     break
                 else:
+                    # Fails - pop number from numbers and stay in while loop.
+                    # Should stay on current position in row
+                    print(f'{num} fails checks. Picking a new number')
+                    print(sep)
+                    grid[rowindex][colindex] = 0
                     attempted.append(num)
-                    # print(f'{num} at ({row}-{col}) is not valid')
-                    # printGrid(grid)
+
+        # Row complete
+        print(f'Row {rowindex} is complete.', 'FINISHED SOLVING' if rowindex == 8 else f'Moving on to row {rowindex}')  
+        print(sep)
+
     return grid
 
 def removeCells(grid, num):
+    #Take copy of grid
+    grid = [row[:] for row in grid]
+
     blanks = 0
+    print(f'Beginning with {blanks} blanks. Entering while loop.')
 
     while True:
+        print(f'Current blanks: {blanks}')
+
+        # Check to see if we have desired blanks
         if blanks == num:
+            print(f'blanks{blanks} = num{num}. Breaking out of while loop. Falling off end of function')
             break
+        
+        # Pick random cell from grid
+        row = random.choice(range(0,9))
+        col = random.choice(range(0,9))
 
-        else:
-            row = random.choice(range(0,9))
-            col = random.choice(range(0,9))
+        print(f'Chosen cell: {row},{col}.')
+        print(f'Current number in cell: {grid[row][col]}')
 
-            if grid[row][col]:
-                grid[row][col] = 0
-                blanks += 1
+        if grid[row][col]:
+            print('Current number is non zero. Making it zero and adding one to blanks')
+            grid[row][col] = 0
+            blanks += 1
+
+        print(sep)
+        printGrid(grid)
+        print(sep)
+
+    return grid
+
 
 if __name__ == '__main__':
+    file = open('output.txt', 'w')
+    sys.stdout = file
 
+    # Generate board
+    print('BEGIN GENERATING PUZZLE')
     grid = fillGrid()
-    removeCells(grid, 30)
+    print(sep)
+
+    # Remove cells
+    print('BEGIN REMOVING CELLS')
+    puzzle = removeCells(grid, 30)
+    print(sep)
+
+    # Solve board
+    print('BEGIN SOLVING')
+    solvedPuzzle = fillGrid(puzzle)
+
+    print(sep)
+    print('FILLED PUZZLE')
     print(sep)
     printGrid(grid)
+    print(sep)
 
-    # file = open('output.txt', 'w')
+    print('INITIAL PUZZLE')
+    print(sep)
+    printGrid(puzzle)
+    print(sep)
 
-    # sys.stdout = file
-    # grid = fillGrid()
-    # print(sep)
-    # printGrid(grid)
+    print('SOLVED PUZZLE')
+    print(sep)
+    printGrid(solvedPuzzle)
+    print(sep)
